704881658ca71f691d125cc686c95d4fc27f64f5
chong.a.liu <chong.a.liu@accenture.com>
2019-11-13 14:51:45 +0800

    add

--- ApimService/src/main/java/com/example/apim/service/controller/AllController.java
+++ ApimService/src/main/java/com/example/apim/service/controller/AllController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,15 +14,17 @@ import com.alibaba.fastjson.JSONObject;
 
 @RestController
 @CrossOrigin
-@RequestMapping("/apim")
-public class ApimController {
+@RequestMapping("{p1}")
+public class AllController {
 
-	protected Logger logger = LoggerFactory.getLogger(ApimController.class);
+	protected Logger logger = LoggerFactory.getLogger(AllController.class);
 
-	@RequestMapping(value = "/service")
+	@RequestMapping(value = "{p2}")
 	public JSONObject service(HttpServletResponse response, 
 			@RequestParam(value = "status" , required = false ) String status, 
-			@RequestParam(value = "delay" , required = false ) String delay)
+			@RequestParam(value = "delay" , required = false ) String delay,
+			@PathVariable(value = "p1" , required = false ) String p1,
+			@PathVariable(value = "p2" , required = false ) String p2)
 					throws NumberFormatException, InterruptedException {
 		this.logger.info("service start");
 		
@@ -36,6 +39,9 @@ public class ApimController {
 			Thread.sleep(Long.parseLong(delay));
 			result.put("delay", delay);
 		}
+		
+		result.put("p1", p1);
+		result.put("p2", p2);
 
 		return result;
 	}
