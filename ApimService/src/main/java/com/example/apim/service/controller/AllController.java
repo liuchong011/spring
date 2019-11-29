b6f867ec8f8ca1032fe34b766c2f9f1613f1177f
chong.a.liu <chong.a.liu@accenture.com>
2019-11-29 11:42:14 +0800

    add 3

--- ApimService/src/main/java/com/example/apim/service/controller/AllController.java
+++ ApimService/src/main/java/com/example/apim/service/controller/AllController.java
@@ -45,5 +45,59 @@ public class AllController {
 
 		return result;
 	}
+	
+	@RequestMapping(value = "")
+	public JSONObject service1(HttpServletResponse response, 
+			@RequestParam(value = "status" , required = false ) String status, 
+			@RequestParam(value = "delay" , required = false ) String delay,
+			@PathVariable(value = "p1" , required = false ) String p1)
+					throws NumberFormatException, InterruptedException {
+		this.logger.info("service start");
+		
+		JSONObject result = new JSONObject();
+
+		if (status != null) {
+			response.setStatus(Integer.parseInt(status));
+			result.put("status", status);
+		}
+		
+		if (delay != null) {
+			Thread.sleep(Long.parseLong(delay));
+			result.put("delay", delay);
+		}
+		
+		result.put("p1", p1);
+
+		return result;
+	}
+	
+	@RequestMapping(value = "{p2}/{p3}")
+	public JSONObject service3(HttpServletResponse response, 
+			@RequestParam(value = "status" , required = false ) String status, 
+			@RequestParam(value = "delay" , required = false ) String delay,
+			@PathVariable(value = "p1" , required = false ) String p1,
+			@PathVariable(value = "p2" , required = false ) String p2,
+			@PathVariable(value = "p3" , required = false ) String p3)
+					throws NumberFormatException, InterruptedException {
+		this.logger.info("service start");
+		
+		JSONObject result = new JSONObject();
+
+		if (status != null) {
+			response.setStatus(Integer.parseInt(status));
+			result.put("status", status);
+		}
+		
+		if (delay != null) {
+			Thread.sleep(Long.parseLong(delay));
+			result.put("delay", delay);
+		}
+		
+		result.put("p1", p1);
+		result.put("p2", p2);
+		result.put("p3", p2);
+
+		return result;
+	}
 
 }
